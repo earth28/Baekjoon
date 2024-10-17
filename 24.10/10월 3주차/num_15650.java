1df486f640a2ebd5237f345782bd57e6b6e8a378
earth28 <jiu9659@gmail.com>
2024-10-17 18:50:20 +0900

    Add files via upload

---
+++ 24.10/10월 3주차/num_15650.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class num_15650 {
+	public static int N, M;
+	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[M];
+		
+		dfs(1, 0);
+		
+		System.out.println(sb);
+	}
+	
+	public static void dfs(int at, int depth) {
+		if (depth == M) {	// 재귀 깊이가 M과 같아지면 배열 출력
+			for (int val : arr) {
+				sb.append(val).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		// at : 현재 위치
+		for (int i = at; i <= N; i++) {
+			arr[depth] = i;
+			dfs(i + 1, depth + 1);
+		}
+	}
+}
